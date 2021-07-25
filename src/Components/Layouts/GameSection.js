3885d301cccd0a956741c9c0952f413d9b6e6412
VivekBcloud <20vivektoppo@gmail.com>
2021-07-25 20:07:18 +0530

    cells editable fixed 🏑

--- src/Components/Layouts/GameSection.js
+++ src/Components/Layouts/GameSection.js
@@ -3,23 +3,36 @@ import { useSudokuContext } from '../../context/SudokuContext';
 import '../../styles/game.scss';
 
 const GameSection = () => {
-  const { cellSelected, setCellSelected, initArray, setInitArray } =
-    useSudokuContext();
+  const {
+    cellSelected,
+    setCellSelected,
+    initArray,
+    setInitArray,
+    editable,
+    setEditable,
+  } = useSudokuContext();
 
   const rows = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
   useEffect(() => {
-    setInitArray([
+    const problemArray = [
       7, 0, 0, 9, 0, 0, 2, 0, 8, 0, 0, 0, 0, 0, 8, 4, 0, 9, 0, 0, 0, 2, 4, 0, 1,
       3, 0, 0, 1, 0, 0, 0, 6, 0, 9, 0, 4, 0, 0, 0, 0, 0, 0, 1, 0, 8, 0, 0, 0, 0,
       3, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 2,
       0, 0, 1, 0, 0, 0,
-    ]);
+    ];
+    setInitArray(problemArray);
+    setEditable(() => {
+      return problemArray.map((item) => {
+        if (item === 0) return true;
+        return false;
+      });
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const _Cells = (indexOfArray, value, box, indx) => {
-    if (value === 0) {
+    if (editable[indexOfArray]) {
       return (
         <td
           key={indx}
@@ -27,7 +40,9 @@ const GameSection = () => {
             cellSelected === indexOfArray ? 'selected-box' : box
           }`}
           onClick={() => setCellSelected(indexOfArray)}
-        ></td>
+        >
+          {value === 0 ? ' ' : value}
+        </td>
       );
     }
     return (
@@ -46,7 +61,7 @@ const GameSection = () => {
                 {rows.map((column, indx) => {
                   const indexOfArray = row * 9 + indx;
                   const value = initArray[indexOfArray];
-                  if (value === 0) {
+                  if (editable[indexOfArray]) {
                     return _Cells(indexOfArray, value, 'blank-box', indx);
                   }
                   return _Cells(indexOfArray, value, 'filled-box', indx);
