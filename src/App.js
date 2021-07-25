59093e8108691a116f376e2f61866df4fb532d29
ashirbad <ashirbadbehera29@gmail.com>
2021-07-26 02:13:40 +0530

    ⚡️ add nav and controles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import './App.scss';
 import Game from './Components/Layouts/Game';
 import { SudokuProvider } from './context/SudokuContext';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Nav from './Components/Layouts/Nav';
+import LearnToPlay from './Components/Layouts/LearnToPlay';
+
 function App() {
   return (
-    <SudokuProvider>
-      <Game />
-    </SudokuProvider>
+    <Router>
+      <SudokuProvider>
+        <Nav />
+        <Route exact path="/" component={Game} />
+        <Route path="/learn" component={LearnToPlay} />
+      </SudokuProvider>
+    </Router>
   );
 }
 
